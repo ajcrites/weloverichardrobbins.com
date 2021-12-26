184a23d8d6153076f3dda13c97f8d081cfef56e0
Andrew Crites <acrites@mobiquityinc.com>
2021-12-26 12:46:13 -0500

    feat(init): baseline for infra

---
+++ infrastructure/main.tf
@@ -0,0 +1,23 @@
+provider "netlify" {
+  token = "${var.netlify_token}"
+}
+
+resource "netlify_deploy_key" "key" {}
+
+resource "netlify_site" netlify_site {
+  name = "${var.app_name}"
+  custom_domain = "${var.site_name}"
+
+  repo {
+    command = "yarn scutage src"
+    deploy_key_id = "${netlify_deploy_key.key.id}"
+    dir = "dist"
+    provider = "github"
+    repo_path = "ajcrites/color-tool"
+    repo_branch = "master"
+  }
+}
+
+terraform {
+  backend "s3" {}
+}
